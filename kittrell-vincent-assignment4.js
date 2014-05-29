01a50a990fc9d48deef6b34282bae3086446ce0b
vskittrell <vskittrell@fullsail.edu>
2014-05-29 18:11:55 -0400

    Added a number problem

--- kittrell-vincent-assignment4.js
+++ kittrell-vincent-assignment4.js
@@ -2,77 +2,130 @@
 //Vincent Kittrell
 //Term 1405
 
+alert("I hope you like to string along.");
+
 //String Problem 1: Phone number
 
-alert ("Need to find out if you have a correct phone number.");
+alert("Need to find out if you have a correct phone number.");
 
-var usrInput = prompt("Please enter a number in the xxx-xxx-xxxx format below: ");
+var userInput = prompt("Please enter a number in the xxx-xxx-xxxx format below: ");
 
-//Phone number format checker --String function:
-var chck = function(usrInput)
-{
+//Phone number verifier
 
-  if(usrInput.charAt(3) !== "-" || usrInput.charAt(7) !== "-"){
-    alert ("Yuck! Can't call you!");
-      return false;
-  }
-  else{
-    alert ("Yay! Can I call you? Maybe?");
-      return true;
-  }
+var ver = function (userInput) {
+
+    if (userInput.charAt(3) !== "-" || userInput.charAt(7) !== "-") {
+        alert("Yuck! Can't call you!");
+        return false;
+    }else{
+        alert("Yay! Can I call you? Maybe?");
+        return true;
+    }
 
 };
 
-var chcking = chck (usrInput);
+var verify = ver(userInput);
 
-console.log(chcking);
+console.log(verify);
 
 
 //String Problem 2: Email Address
 
 //Global variables
+
 var usrInput = prompt("Enter an email address in the aaa@bbb.ccc format");
 
 //String
-var emailChck = function(usrInput){
 
-		if(usrInput.search("@") === -1 || usrInput.charAt(usrInput.length-4) !== "."){
-alert ("You've got mail!");
+var myemail = function(userInput){
+
+		if(userInput.search("@") === -1 || usrInput.charAt(usrInput.length-4) !== "."){
+alert ("No fan mail for you. :(");
 			return false;
 		}
 		else{
-alert ("No fan mail for you. :(");
+alert ("You've got mail!");
 			return true;
 		}
 
 };
-var myReturnVal = emailChck(usrInput);
+var myReturnVal = myemail(userInput);
 
 console.log(myReturnVal);
 
 //String Problem 3: Website URL
 
 //Global variable
-var usrInput = prompt("Enter a URL in the format http:// or https:// ");
 
-//URL protocol verifier function
-var urlChck = function(usrInput){
+var userInput = prompt("Do you have a website?");
 
-  var urlProtocol = usrInput.substr(0,7);
-  var urlSecProtocol = usrInput.substr(0,8);
+//Website function
+var wscheck = function(userInput){
+
+  var urlProtocol = userInput.substr(0,7);
+  var urlSecProtocol = userInput.substr(0,8);
 
   if(urlProtocol ==="http://" || urlSecProtocol === "https://"){
-alert ("Surfs up,baby!");
-    return true;
+            alert ("Surfs up,baby!");
+            return true;
   }
-  else{
-alert ("Whoah! Wipe out, bro.");
-    return false;
+  else {
+      alert("Whoah! Wipe out, bro.");
+      return false;
   }
 
 };
 
-var myReturnVal = urlChck(usrInput);
+var myReturn = wscheck(userInput);
 
-console.log(myReturnVal);
+console.log(myReturn);
+
+//String Problem 4: Upper Case the title
+
+//Global Variables
+
+//String Problem 5:
+
+alert("01010110101010101011010101010101010101........Mathman!");
+
+//Number Problem 1:
+
+//Number Problem 2:
+
+//Number Problem 3: Two dates
+
+//Variables
+var dateDif = 0
+
+var theDate = function(date) {
+    var date1 = prompt("What is your birthday?\n ");
+    var date2 = prompt("Enter somebody else's birthday.\n ");
+    var date3 = new Date();
+    var responseType = "Hour";
+        
+    responseType = prompt("Do you want know how much older or younger you are? You only get to know in days or hours. Mwhahahaha!","days or hours");
+    if (responseType === "hours") {
+        date3 = ((date2*1)-(date1*1))/3600000;
+        date3 = date3.toFixed(2);
+        alert("The difference between the two dates is " + date3 + " hours.\n ");
+        return date3;
+}else if (responseType === "days") {
+        date3 = ((date2*1)-(date1*1))/86400000;
+        date3 = Math.abs(date3);
+        date3 = date3.toFixed(2);
+        alert("The difference between the two dates is " + date3 + " days.\n ");
+        return date3;
+}else{
+        alert("Some one isn't listening....Shame shame.");
+};
+
+};
+
+dateDiff = theDate();
+alert("Look at that difference!");
+
+//Number Problem 4:
+
+
+
 
