4512c516d606c8e76e629c87e9faa0d8ae99547c
vskittrell <vskittrell@fullsail.edu>
2014-05-29 13:25:07 -0400

    Added alerts to string returns

--- kittrell-vincent-assignment4.js
+++ kittrell-vincent-assignment4.js
@@ -4,6 +4,8 @@
 
 //String Problem 1: Phone number
 
+alert ("Need to find out if you have a correct phone number.");
+
 var usrInput = prompt("Please enter a number in the xxx-xxx-xxxx format below: ");
 
 //Phone number format checker --String function:
@@ -11,10 +13,12 @@ var chck = function(usrInput)
 {
 
   if(usrInput.charAt(3) !== "-" || usrInput.charAt(7) !== "-"){
-    return false;
+    alert ("Yuck! Can't call you!");
+      return false;
   }
   else{
-    return true;
+    alert ("Yay! Can I call you? Maybe?");
+      return true;
   }
 
 };
